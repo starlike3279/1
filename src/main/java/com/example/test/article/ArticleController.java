eb475310f856b79eaa47f35476a6f566e8df2813
taeho5825 <xogh332@naver.com>
2024-09-19 12:49:38 +0900

    1

--- src/main/java/com/example/test/article/ArticleController.java
+++ src/main/java/com/example/test/article/ArticleController.java
@@ -1,10 +1,11 @@
 package com.example.test.article;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +24,26 @@ public class ArticleController {
     }
 
     @GetMapping("/create")
+    public String articleCreate(){
+        return "article_form";
+    }
 
+    @PostMapping("/create")
+    public String articleCreate(@RequestParam(value="title") String title,
+                                @RequestParam(value="content") String content){
+        this.articleService.create(title, content);
+        return "redirect:/article/list";
+    }
 
+    @GetMapping (value = "/detail/{id}")
+    public String create(Model model, @PathVariable("id") Integer id,
+                         @Valid ArticleForm articleForm, BindingResult bindingResult){
+        Article article = this.articleService.getArticle(id);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("articleForm", new ArticleForm());
+            return "article_detail";
+        }
+        this.articleService.create(articleForm.getTitle(), articleForm.getContent());
+        return String.format("redirect:/article/detail/%s", id);
+    }
 }
